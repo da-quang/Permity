808db929ecd15a07b59c1bcaa3c893f47fe26297
jokley <reinhard_juen@vol.at>
2021-12-09 11:48:12 +0100

    Update Delete.js
    Die Daten können so von ./Index nicht importiert werden.

--- frontend/pages/startseite/Delete.js
+++ frontend/pages/startseite/Delete.js
@@ -1,24 +1,30 @@
-import { data } from 'index'
+//import { data } from './index'
 
-export default function handler(req, res) {
-    const { auftragID } = req.query
+// export default function handler(req, res) {
+//     const { auftragID } = req.query
 
-    if (req.method === 'GET') {
+//     if (req.method === 'GET') {
 
 
-        const auftrag = data.find((auftrag) => auftrag.ID === parseInt(auftragID))
-        res.status(200).json(auftrag)
-    } else if (req.method === 'DELETE') {
-        const deletedAuftrag = data.find(
-            (auftrag) => auftrag.ID === parseInt(auftragID)
-        )
+//         const auftrag = data.find((auftrag) => auftrag.ID === parseInt(auftragID))
+//         res.status(200).json(auftrag)
+//     } else if (req.method === 'DELETE') {
+//         const deletedAuftrag = data.find(
+//             (auftrag) => auftrag.ID === parseInt(auftragID)
+//         )
 
-        const index = data.findIndex(
-            auftrag => auftrag.ID === parseInt(auftragID)
-        )
-        data.splice(index, 1)
+//         const index = data.findIndex(
+//             auftrag => auftrag.ID === parseInt(auftragID)
+//         )
+//         data.splice(index, 1)
 
-        res.status(200).json(deletedAuftrag)
+//         res.status(200).json(deletedAuftrag)
 
-    }
-}
+//     }
+// }
+
+const ContactPage = () => {
+    return <h1>This is contact page</h1>;
+  };
+  
+  export default ContactPage;

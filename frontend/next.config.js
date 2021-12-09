1e2aa6f4b6231df94d9b98b561466affc0ec1081
jokley <reinhard_juen@vol.at>
2021-12-09 08:01:15 +0100

    Update next.config.js
    Ignore eslint during builds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,4 +2,10 @@ module.exports = {
   env: {
     customKey: 'my-value',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  optimizeFonts: false,
 }

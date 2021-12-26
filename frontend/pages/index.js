0ce59d80d905873ab6696fe7b26e3d96f21eda47
da-quang <da.quang@outlook.com>
2021-12-26 17:55:23 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,42 +1,6 @@
-import { useRouter } from 'next/router';
-import { makeStyles } from '@mui/styles';
-import Button from '@mui/material/Button';
-
-
-console.log("--> Start")
 export default function Start() {
-    const router = useRouter()
-
-    function start(){
-      router.push(`/mitarbeiter/login`) 
-    }
-
-    const classes = useStyles();
     return(
-        <div className={classes.b}>
-          <Button className={classes.a} onClick={() => start()}>Starten</Button>
-        </div>
+      <a href="https://palmiest-hornet-1388.dataplicity.io/ui/mitarbeiter/login">Visit Permity</a>
     )
 }
-
-const useStyles = makeStyles({
-    a: {
-      background: 'linear-gradient(45deg, #143968  30%, #143968  90%)',
-      borderRadius: 3,
-      boxShadow: '0 2px 5px 2px rgba(50, 50, 50, .3)',
-      color: 'white',
-      height: 80,
-      width: "70%",
-      padding: '0 30px',
-      textAlign: 'center',
-      marginTop: "10%",
-      
-    },
-
-    b: {
-        display: 'block',
-        marginLeft: 'auto',
-        marginTop: 'auto'
-    },
-})
 

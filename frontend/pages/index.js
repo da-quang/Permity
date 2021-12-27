c6bb3afd7a371c9d50e0a25d66d9afe27d4cb0bd
da-quang <da.quang@outlook.com>
2021-12-27 15:30:27 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+
 export default function Start() {
     return(
-      <a href="/mitarbeiter/login">Visit Permity</a>
+      <Link href="/mitarbeiter/login">
+        <Button>Visit Permity</Button>
+      </Link>
     )
 }
 

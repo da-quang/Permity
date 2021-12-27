8979802ea4abc2d9623607e1f2cbaaa584132275
da-quang <da.quang@outlook.com>
2021-12-27 15:27:57 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
 export default function Start() {
     return(
-      <a href="http://localhost:3000/mitarbeiter/login">Visit Permity</a>
+      <a href="/mitarbeiter/login">Visit Permity</a>
     )
 }
 

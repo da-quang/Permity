1c5ddaa3545b6db680cc83bd426d15f447334989
da-quang <da.quang@outlook.com>
2021-12-27 18:46:46 +0100

    a

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,27 @@
 export default function Start() {
     return(
       <div>
+        <div>
           <a href="https://palmiest-hornet-1388.dataplicity.io/ui/mitarbeiter/login">Visit Permity</a>
+        </div>
+        <div>
           <a href="https://palmiest-hornet-1388.dataplicity.io/mitarbeiter/login">Visit Permity</a>
-          <a href="https://172.16.238.13/ui/mitarbeiter/login">Visit Permity</a>
-          <a href="https://172.16.238.13:3000/ui/mitarbeiter/login">Visit Permity</a>
+        </div>
+        <div>
+         <a href="https://172.16.238.13/ui/mitarbeiter/login">Visit Permity</a>
+        </div>
+        <div>
+         <a href="https://172.16.238.13:3000/ui/mitarbeiter/login">Visit Permity</a>
+        </div>
+        <div>
           <a href="https://172.16.238.13:3000/mitarbeiter/login">Visit Permity</a>
-          <a href="/mitarbeiter/login">Visit Permity</a>
+        </div>
+        <div>
+         <a href="/mitarbeiter/login">Visit Permity</a>
+        </div>
+        <div>
           <a href="./mitarbeiter/login">Visit Permity</a>
+        </div>
       </div>
     )
 }

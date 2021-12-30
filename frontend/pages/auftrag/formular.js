898c44f38c174ebc5d036e3c039aeb7e0b8d0ec3
da-quang <67861088+da-quang@users.noreply.github.com>
2021-12-30 23:19:51 +0100

    Update formular.js

--- frontend/pages/auftrag/formular.js
+++ frontend/pages/auftrag/formular.js
@@ -1,7 +1,6 @@
-import { Button,TextField, Grid, Fab, Link, Typography } from "@material-ui/core";
-import React, { useState, useEffect } from 'react';
+import { Button, TextField, Grid, Fab, Link, Typography } from "@material-ui/core";
+import React, { Fragment, useState, useEffect } from 'react';
 import MomentUtils from '@date-io/moment';
-import { DatePicker, TimePicker, DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { Autocomplete } from "@mui/material";
@@ -11,6 +10,8 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AddIcon from '@mui/icons-material/Add';
 import Stack from '@mui/material/Stack';
 import MobileDateTimePicker from '@mui/lab/MobileDateTimePicker';
+import TimePicker from '@mui/lab/TimePicker';
+import DateTimePicker from '@mui/lab/DateTimePicker';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { delLocale } from "next/dist/shared/lib/router/router";
@@ -20,7 +21,10 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Router from "next/router";
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-
+import Box from '@mui/material/Box';
+import CreateIcon from '@mui/icons-material/Create';
+import Create from "@mui/icons-material/Create";
+import { getDate, getHours } from "date-fns";
 console.log("--> Formular")
 
 
@@ -32,29 +36,120 @@ export default function Formular() {
     const classes = useStyles();
     // const [data, setData] = useState([]);
 
+    let [ksv, setKsv] = useState('')
     let auftraggeber = query.param2
     let [auftragnehmer, setAuftragnehmer] = useState('')
+    let [empfänger, setEmpfänger] = useState('')
     let [sperren, setSperren] = useState('')
     let [kommentar, setKommentar] = useState('')
     let [von, setVon] = useState(new Date())
     let [bis, setBis] = useState(new Date())
     let status = 'offen'
+
+    const [data1, setData1] = useState([]);
     const [data2, setData2] = useState([]);
- 
+    const [data3, setData3] = useState([]);
+    const [data4, setData4] = useState([]);
+    const [data5, setData5] = useState([]);
+    const [data6, setData6] = useState([]);
+    const [data7, setData7] = useState([]);
+    const [data8, setData8] = useState([]);
+    const [data9, setData9] = useState([]);
+    const [data10, setData10] = useState([]);
+    const [data11, setData11] = useState([]);
 
+    let betreff = "Freigabe"
+    let mailtext = "Bitte erledigen"
 
-    function create() {
-        axios.post('https://palmiest-hornet-1388.dataplicity.io/api/api/Auftrag/create', null, { params: { ksv, auftraggeber, auftragnehmer, sperren, kommentar, von, bis, status } })
-            .then(res => {
-                console.log(res)
+    // const formData = new FormData();
+    // formData.append('empfänger', 'hodzic.amir66@gmail.com');
+    // formData.append('betreff', 'Sees');
+    // formData.append('mailtext', 'hallo');
+    // formData.append('attachments', '');
 
-            })
-            .then((data) => setData(data));
+   
+
+    const CREATE = async () => {
+        const response = await fetch(`http://localhost:8090/api/Auftrag/create?ksv=${ksv}-13&auftraggeber=Riegler%20Thomas&auftragnehmer=Boch%20Walter&sperren=Freigabe%20zur%20Arbeit&kommentar=Gut%20machen&von=2021.12.30&bis=2021.12.31`, {
+            method: 'POST'
+        })
+        const data = await response.json()
+        console.log(data)
+        Router.reload()
     }
+
+
 
     let kurzzeichen = query.param
 
+
     let loaded = false
+
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/Mitarbeiter/all`)
+            .then((response) => response.json())
+            .then((data2) => setData2(data2));
+    }, []);
+
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ebene=1`)
+            .then((response) => response.json())
+            .then((data3) => setData3(data3));
+    }, []);
+
+    function loadEbene2() {
+
+        useEffect(() => {
+            fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=2`)
+                .then((response) => response.json())
+                .then((data4) => setData4(data4));
+        }, []);
+
+    }
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=3`)
+            .then((response) => response.json())
+            .then((data5) => setData5(data5));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=4`)
+            .then((response) => response.json())
+            .then((data6) => setData6(data6));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=5`)
+            .then((response) => response.json())
+            .then((data7) => setData7(data7));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=6`)
+            .then((response) => response.json())
+            .then((data8) => setData8(data8));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=7`)
+            .then((response) => response.json())
+            .then((data9) => setData9(data9));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=8`)
+            .then((response) => response.json())
+            .then((data10) => setData10(data10));
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:8090/api/KSV/select?ksv=${ksv1}&ebene=9`)
+            .then((response) => response.json())
+            .then((data11) => setData11(data11));
+    }, []);
 
     //Menü
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -69,30 +164,48 @@ export default function Formular() {
     const router = useRouter()
 
     const [ksv1, setKsv1] = useState([]);
+
+
+
+
+
+
+
     const handleLoad1 = (event, value) => setKsv1(value);
 
     const [value, setValue] = useState(new Date());
     const [value1, setValue1] = useState(new Date());
-    
-    const [kurzzeichenOrName, setKurzzeichenOrName] = useState([]);
-    function findMitarbeiter() {
-        axios.get('https://palmiest-hornet-1388.dataplicity.io/api/api/Mitarbeiter/find', { params: { kurzzeichenOrName } })
-        .then(res => {
-            console.log(res.data)
-        })
-    }
+    const [x, setx] = useState("");
+    const handleChange = e => {
+        console.log(`${e.target.value}`);
+        setx(e.target.value)
+    };
 
-    const [ksv, setKsv] = useState([]);
-    function getKsv() {
-        axios.get('https://palmiest-hornet-1388.dataplicity.io/api/api/KSV/ksv', { params: { ksv } })
-        .then(res => {
-            console.log(res.data)
-        })
-    }
+    let VON = `${value.getFullYear()}.${value.getMonth() + 1}.${value.getDate()}`;
+    let BIS = `${value1.getFullYear()}.${value1.getMonth() + 1}.${value1.getDate()}`;
+    // ${value.getHours()}:${value.getMinutes()}
+console.log(VON)
+console.log(BIS)
 
+    let [KSV, setKSV] = useState('')
+    let [KOMMENTAR, setKOMMENTAR] = useState('')
+    let [AUFTRAGNEHMER, setAUFTRAGNEHMER] = useState('')
+    let [SPERREN, setSPERREN] = useState('')
+    let AUFTRAGGEBER = query.param2
+
+    const CREATE2 = async () => {
+        const response = await fetch(`http://localhost:8090/api/Auftrag/create?ksv=${KSV}&auftraggeber=${AUFTRAGGEBER}&auftragnehmer=${AUFTRAGNEHMER}&sperren=${SPERREN}&kommentar=${KOMMENTAR}&von=${VON}&bis=${BIS}`, {
+            method: 'POST'
+        })
+        const data = await response.json()
+        console.log(data)
+        Router.reload()
+    }
     return (
         <form className={classes.h}>
+
             <div>
+
                 <div className={classes.e}>
                     <div>
                         <Button color="inherit" className={classes.BTN}
@@ -102,7 +215,7 @@ export default function Formular() {
                             aria-expanded={open ? 'true' : undefined}
                             onClick={handleClick}
                         >
-                        <MenuIcon fontSize="large" />
+                            <MenuIcon fontSize="large" />
                         </Button>
                         <Menu
                             id="basic-menu"
@@ -111,7 +224,8 @@ export default function Formular() {
                             onClose={handleClose}
                             MenuListProps={{
                                 'aria-labelledby': 'basic-button',
-                            }}>
+                            }}
+                        >
                             <MenuItem onClick={() => router.push(`/Mitarbeiter/login?param`)}><LogoutIcon />Logout </MenuItem>
                             <MenuItem onClick={() => router.back()}><HomeIcon />Startseite</MenuItem>
                         </Menu>
@@ -119,63 +233,156 @@ export default function Formular() {
                     <Typography variant="h4" className={classes.typoh4}> Auftrag </Typography>
                     <Typography variant="h6" className={classes.typoh6}>{query.param}</Typography>
                 </div>
+
+                <Box position="fixed" className={classes.Fab} sx={{ '& > :not(style)': { m: 1 } }}>
+
+
+                    <Fab variant="extended" color="primary" aria-label="add">
+                        <CreateIcon /> Unterschreiben
+                    </Fab>
+
+                    <Fab onClick={() => CREATE2()} color="secondary" aria-label="add">
+                        <AddIcon />
+                    </Fab>
+
+                </Box>
             </div>
             <Grid container spacing={2}>
+
+
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                     <Grid item xs={6}>
                         <Stack>
                             <MobileDateTimePicker
+                                mask="____/__/__"
                                 ampm={false}
                                 value={value}
                                 onChange={(newValue) => {
                                     setValue(newValue)
                                 }}
                                 label="Von"
-                                inputFormat="dd/MM/yyyy hh:mm"
+                                inputFormat="yyyy.MM.dd HH:mm"
+
                                 renderInput={(params) => <TextField onChange={e => setVon(e.target.value)} variant="outlined" {...params} />}
+
                             />
+
                         </Stack>
+
                     </Grid>
+
+
 
                     <Grid item xs={6}>
                         <Stack>
+
                             <MobileDateTimePicker
+
                                 ampm={false}
                                 label="24hours"
-                                disableFuture
+                               
                                 value={value1}
                                 onChange={(newValue1) => {
                                     setValue1(newValue1)
                                 }}
                                 label="Bis"
-                                inputFormat="dd/MM/yyyy hh:mm"
+                                inputFormat="yyyy/MM/dd HH:mm"
+
                                 renderInput={(params) => <TextField onChange={e => setBis(e.target.value)} variant="outlined" {...params} />}
+
                             />
+
                         </Stack>
                     </Grid>
+
+
+
                 </LocalizationProvider>
 
 
                 <Grid item xs={12}>
-                    <TextField fullWidth variant="outlined" label="Kommentar" onChange={e => setKommentar(e.target.value)}></TextField>
+                    <TextField  fullWidth variant="outlined" label="Kommentar" onChange={e => setKOMMENTAR(e.target.value)}></TextField>
                 </Grid>
-            
                 <Grid item xs={6}>
-                    <TextField variant="outlined" label="Mitarbeiter/in" onChange={e => setKurzzeichenOrName(e.target.value)}></TextField>
-                    <Button onClick={() => findMitarbeiter()}>Suchen</Button>
+                <TextField  fullWidth variant="outlined" label="Auftragnehmer" onChange={e => setAUFTRAGNEHMER(e.target.value)}></TextField>
+                    
                 </Grid>
 
                 <Grid item xs={6}>
-                    <TextField variant="outlined" label="KSV" onChange={e => setKsv(e.target.value)}></TextField>
-                    <Button onClick={() => getKsv()}>Suchen</Button>
+                <TextField  fullWidth variant="outlined" label="Sperren" onChange={e => setSPERREN(e.target.value)}></TextField>
+                    
                 </Grid>
+                <Grid item xs={6}>
+
+                <Grid item xs={12}>
+                    <TextField  fullWidth variant="outlined" label="KSV" onChange={e => setKSV(e.target.value)}></TextField>
+                </Grid>
+
+                {/* <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={data3}
+                        getOptionLabel={(option) => option.KSV}
+                        renderInput={(params) => (<TextField {...params} variant="outlined" onChange={() => setKSV(e.target.value)} label="Ksv"></TextField>)}
+                        isOptionEqualToValue={(option, value) => option.KSV === value.KSV}
+                    /> */}
+                </Grid>
+                {/* <Grid item xs={6}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={data5}
+                        getOptionLabel={(option) => option.KSV}
+                        renderInput={(params) => (<TextField {...params} variant="outlined" label="Ksv" ></TextField>)}
+                        isOptionEqualToValue={(option, value) => option.KSV === value.KSV}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={data6}
+                        getOptionLabel={(option) => option.KSV}
+                        renderInput={(params) => (<TextField {...params} variant="outlined" label="Ksv" ></TextField>)}
+                        isOptionEqualToValue={(option, value) => option.KSV === value.KSV}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={data7}
+                        getOptionLabel={(option) => option.KSV}
+                        renderInput={(params) => (<TextField {...params} variant="outlined" label="Ksv" onChange={e => setKsv1(e.target.value)}></TextField>)}
+                        isOptionEqualToValue={(option, value) => option.KSV === value.KSV}
+                    />
+                </Grid> */}
+
+
+
+                
             </Grid>
-        
-            <Fab onClick={() => create()} color="secondary" className={classes.Fab} aria-label="add">
-                <AddIcon />
-            </Fab>
+
+
+            {/* <Grid item xs={8}>
+                    <TextField fullWidth label="Auftragnehmer" onChange={e => setAuftragnehmer(e.target.value)}></TextField>
+                </Grid >
+                <Grid item xs={6}>
+                    <TextField fullWidth label="Sperren" onChange={e => setSperren(e.target.value)}></TextField>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth label="Kommentar" onChange={e => setKommentar(e.target.value)}></TextField>
+                </Grid >
+                <Grid item xs={6}>
+                    <TextField fullWidth label="Von" onChange={e => setVon(e.target.value)}></TextField>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth label="Bis" onChange={e => setBis(e.target.value)}></TextField>
+                </Grid> */}
+
         </form>
     )
+
 }
 
 
@@ -183,26 +390,8 @@ export default function Formular() {
 
 
 const useStyles = makeStyles({
-    a: {
-        background: 'linear-gradient(45deg, #0288d1 30%, #03a9f4 90%)',
-        borderRadius: 3,
-        boxShadow: '0 2px 5px 2px rgba(70, 175, 219, .3)',
-        color: 'white',
-        height: 80,
-        width: "70%",
-        padding: '0 30px',
-        textAlign: 'center',
-        marginTop: 40,
-        fontSize: 20,
-    },
 
-    ChangeBTN: {
-        width: 2,
-    },
 
-    DeleteBTN: {
-
-    },
 
 
     typoh4: {
@@ -222,10 +411,11 @@ const useStyles = makeStyles({
     },
 
     Fab: {
-        position: 'relative',
-        left: "80%",
-        background: 'red',
+        position: "fixed",
+        right: "8%",
+        bottom: "5%",
 
+        zIndex: "999",
     },
 
     BTN: {
@@ -255,12 +445,10 @@ const useStyles = makeStyles({
         background: 'linear-gradient(45deg, #143968 30%, #143968 90%)',
         boxShadow: '0 3px 5px 2px rgba(20, 57, 104, .3)',
         marginTop: 0,
-        paddingTop: 60,
+        paddingTop: 15,
         marginBottom: "5%",
-
-        height: 110,
+        height: 60,
         color: 'white',
-
         borderBottomLeftRadius: 15,
         borderBottomRightRadius: 15,
         justifyContent: 'space-between',
@@ -269,39 +457,11 @@ const useStyles = makeStyles({
 
 
 
-    g: {
-        borderBottomLeftRadius: 15,
-        borderBottomRightRadius: 15,
-        borderTopLeftRadius: 15,
-        borderTopRightRadius: 15,
-        background: '#143968',
-        marginRight: "10%",
-        marginLeft: "10%",
-        color: "white",
-    },
-
-    summary: {
-        fontWeight: "bold",
-        height: 45,
-        lineHeight: 3,
-        display: "flex",
-        justifyContent: 'space-between',
 
 
-    },
-
-    p: {
-        borderBottom: "solid",
-        width: "85%",
-        marginTop: 10,
 
 
-    },
 
-    kommi: {
-        borderBottom: "solid",
-        width: "85%",
-        wordWrap: "break-word",
-    },
+
 
 });

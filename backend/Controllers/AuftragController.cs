1b8386e2e163d463532454b45ae41f33cbc1ea2b
da-quang <da.quang@outlook.com>
2022-01-03 02:55:50 +0100

    .

--- backend/Controllers/AuftragController.cs
+++ backend/Controllers/AuftragController.cs
@@ -23,44 +23,18 @@ namespace backend.Controllers
 
         private async Task Confirmation(int id)
         {
-            Task<int> BestätigtTask = BestätigtAsync(id);
-            Task<int> nichtAngenommenTask = nichtAngenommenAsync(id);
-        }
-
-        private async Task<int> BestätigtAsync(int id)
-        {
-            do
+            await Task.Run(() =>
             {
                 Task.Delay(10000).Wait();
                 string sqlDataSource = _configuration.GetConnectionString("AppCon");
                 using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
                 {
                     myCon.Open();
-                    NpgsqlCommand query = new NpgsqlCommand(@"select exists (select ""ID"" from ""Auftrag"" where ""ID"" = @id and ""STATUS"" = 'Bestätigt')", myCon);
+                    NpgsqlCommand query = new NpgsqlCommand(@"update ""Auftrag"" set ""STATUS"" = 'nicht angenommen' where ""ID"" = @id and ""STATUS"" = 'Offen'", myCon);
                     query.Parameters.AddWithValue("@id", id);
-
-                    Boolean flag = (Boolean)query.ExecuteScalar();
-                    if (flag == true)
-                    {
-                        break;
-                    }
+                    query.ExecuteScalar();
                 }
-            } while (true);
-            return 0;
-        }
-
-        private async Task<int> nichtAngenommenAsync(int id)
-        {
-            Task.Delay(20000).Wait();
-            string sqlDataSource = _configuration.GetConnectionString("AppCon");
-            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                NpgsqlCommand query = new NpgsqlCommand(@"update ""Auftrag"" set ""STATUS"" = 'nicht angenommen' where ""ID"" = @id and ""STATUS"" = 'Offen'", myCon);
-                query.Parameters.AddWithValue("@id", id);
-                query.ExecuteScalar();
-            }
-            return 0;
+            });
         }
 
         private int getId(string name)

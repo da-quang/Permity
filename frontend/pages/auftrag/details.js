2a31d01456f1e608e47c7d6f53fb2d43c5930c16
da-quang <da.quang@outlook.com>
2022-01-05 22:51:05 +0100

    Update details.js

--- frontend/pages/auftrag/details.js
+++ frontend/pages/auftrag/details.js
@@ -24,3 +24,59 @@ export default function Start() {
         
     )
 }
+
+const useStyles = makeStyles({
+    a: {
+      background: 'linear-gradient(45deg, #143968  30%, #143968  90%)',
+      borderRadius: 3,
+      boxShadow: '0 2px 5px 2px rgba(50, 50, 50, .3)',
+      color: 'white',
+      height: 80,
+      width: "70%",
+      padding: '0 30px',
+      textAlign: 'center',
+      marginTop: "10%",
+      
+    },
+
+    b: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginTop: 'auto'
+    },
+
+    c: {
+        display: 'inline-block'
+    },
+
+    d: {
+        color: "green",
+        textAlign: 'center',
+        marginTop: 40,
+    },
+
+    e: {
+        background: 'linear-gradient(45deg, #143968  30%, #143968  90%)',
+        boxShadow: '0 3px 5px 2px rgba(70, 175, 219, .3)',
+        marginTop: 0,
+        paddingTop: 60,
+        marginBottom: 100,
+        fontSize: 40,
+        height: 110,
+        color: 'white',
+        fontWeight: 'bold',
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15,
+    },
+
+    f: {
+        background: 'linear-gradient(45deg, #455a64 30%, #546e7a 90%)',
+        boxShadow: '0 3px 5px 2px rgba(70, 175, 219, .3)',
+        height: 75,
+    },
+
+    g: {
+        marginTop:"40%"
+    },
+})
+
